708974d0b951c0a24bfd1e20e179b1e2fa3c9fa5
AnLaxina <anilovlaxina@gmail.com>
2024-11-25 10:28:00 -0800

    Implement getComputerChoice

--- JS/logic.js
+++ JS/logic.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+// Function that returns the "computer's" choice.
+// Returns a random string: "rock", "paper", "scissors"
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    console.log(randomNumber);
+    if (randomNumber === 0) {
+        return "rock";
+    }
+    else if (randomNumber === 1) {
+        return "paper";
+    }
+    if (randomNumber === 2) {
+        return "scissors";
+    }
+}
+
+
